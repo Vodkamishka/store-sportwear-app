9cc447bc8aa3db9f2f45514134914fb6694fd2de
Vodkamishka <andrtomsk@yandex.com>
2019-03-31 14:02:06 +0700

    default

--- src/components/Default.js
+++ src/components/Default.js
@@ -6,7 +6,23 @@ export default class Default extends Component {
   render() {
     return (
       <DefaultWrapper>
-        <h1>page not found</h1>
+        <div className = "container">
+        <div className  = "row">
+        <div classname  = "col-10 mx-auto text-center text-title text-uppercase pt-5">
+        <h1 className = "display-3">404</h1>
+        <h1>error</h1>
+        <h2>page not found</h2>
+        <h3>
+         teh requested URL
+         <span className = "text-danger">
+         {this.props.location.pathname}
+         </span>{" "}
+         was not found
+        </h3>
+        </div>
+        </div>
+        </div>
+        
       </DefaultWrapper>
     )
   }
@@ -19,5 +35,4 @@ height: 100vh;
 width: 1344px;
 display: flex;
 align-items: center;
-text-transform: capitalize;
 `;
